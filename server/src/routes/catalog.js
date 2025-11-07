d860cee929469425388f857d909606249836632b
mvrzan <mvrzan@icloud.com>
2025-11-07 15:56:55 -0500

    add sendMessage route to catalogRoutes

--- server/src/routes/catalog.js
+++ server/src/routes/catalog.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import startSession from "../controllers/start-session.js";
+import sendMessage from "../controllers/send-message.js";
 
 const catalogRoutes = Router();
 
 catalogRoutes.get("/api/v1/start-session", startSession);
+catalogRoutes.post("/api/v1/send-message", sendMessage);
 
 export default catalogRoutes;
