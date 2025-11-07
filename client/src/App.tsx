08bebd17b5e70d43d68d0668fe1f5e6ce802d89d
mvrzan <mvrzan@icloud.com>
2025-11-07 09:56:52 -0500

    remove background color from App component for improved styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -72,7 +72,7 @@ function App() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col">
+    <div className="min-h-screen flex flex-col">
       <Header />
 
       <main className="flex-1">
