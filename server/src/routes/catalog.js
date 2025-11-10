360b7ffa33c9beab0a5c30a514cd156b7696f5dd
mvrzan <mvrzan@icloud.com>
2025-11-10 15:00:45 -0500

    add validateSignature middleware to catalog routes for enhanced security

--- server/src/routes/catalog.js
+++ server/src/routes/catalog.js
@@ -2,11 +2,12 @@ import { Router } from "express";
 import startSession from "../controllers/start-session.js";
 import sendMessage from "../controllers/send-message.js";
 import deleteSession from "../controllers/delete-session.js";
+import { validateSignature } from "../middleware/validateSignature.js";
 
 const catalogRoutes = Router();
 
-catalogRoutes.get("/api/v1/start-session", startSession);
-catalogRoutes.post("/api/v1/send-message", sendMessage);
-catalogRoutes.delete("/api/v1/delete-session", deleteSession);
+catalogRoutes.get("/api/v1/start-session", validateSignature, startSession);
+catalogRoutes.post("/api/v1/send-message", validateSignature, sendMessage);
+catalogRoutes.delete("/api/v1/delete-session", validateSignature, deleteSession);
 
 export default catalogRoutes;
