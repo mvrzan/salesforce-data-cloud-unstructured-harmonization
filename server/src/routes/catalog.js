b3964bf23872d9c039022237bb4c18c4a5608690
mvrzan <mvrzan@icloud.com>
2025-11-10 12:06:49 -0500

    add delete session route to catalog API

--- server/src/routes/catalog.js
+++ server/src/routes/catalog.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import startSession from "../controllers/start-session.js";
 import sendMessage from "../controllers/send-message.js";
+import deleteSession from "../controllers/delete-session.js";
 
 const catalogRoutes = Router();
 
 catalogRoutes.get("/api/v1/start-session", startSession);
 catalogRoutes.post("/api/v1/send-message", sendMessage);
+catalogRoutes.delete("/api/v1/delete-session", deleteSession);
 
 export default catalogRoutes;
