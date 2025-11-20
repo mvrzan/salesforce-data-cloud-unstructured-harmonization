4dc2276d6b0fc3e512b02da1d0489a5f08c2429e
mvrzan <mvrzan@icloud.com>
2025-11-19 21:29:05 -0500

    add getHudmo route to catalog for fetching HUDMO data

--- server/src/routes/catalog.js
+++ server/src/routes/catalog.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import startSession from "../controllers/start-session.js";
 import sendMessage from "../controllers/send-message.js";
 import deleteSession from "../controllers/delete-session.js";
+import getHudmo from "../controllers/get-hudmo.js";
 import { validateSignature } from "../middleware/validateSignature.js";
 
 const catalogRoutes = Router();
@@ -9,5 +10,6 @@ const catalogRoutes = Router();
 catalogRoutes.get("/api/v1/start-session", validateSignature, startSession);
 catalogRoutes.post("/api/v1/send-message", validateSignature, sendMessage);
 catalogRoutes.delete("/api/v1/delete-session", validateSignature, deleteSession);
+catalogRoutes.post("/api/v1/get-hudmo", validateSignature, getHudmo);
 
 export default catalogRoutes;
